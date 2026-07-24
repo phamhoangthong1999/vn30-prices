6e609f3b41da1f3b175f280455e6923a089a94e6
phamhoangthong1999 <93709376+phamhoangthong1999@users.noreply.github.com>
2026-07-24 10:36:49 +0700

    .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,7 @@ module.exports = function (eleventyConfig) {
   });
 
   // Copy các file/tài nguyên tĩnh nguyên trạng, không qua xử lý template
+  eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("index.html");
